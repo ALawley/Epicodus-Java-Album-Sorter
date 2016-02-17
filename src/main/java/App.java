d7258e2ac4aebfc7a9dcaf5ef8d51b7704cd5f2c
Anna Lawley and Ramon de la Cruz <ALawley1@gmail.com>
2016-02-17 13:22:49 -0800

    build front end for displaying albums

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,45 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/albums", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("albums", Album.all());
+      model.put("template", "templates/albums.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("albums/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/album-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
 
+    post("/albums", (request,response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String description = request.queryParams("title");
+      Album newAlbum = new Album(description);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/albums/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      Album album = Album.find(Integer.parseInt(request.params(":id")));
+      model.put("album", album);
+      model.put("template", "templates/album.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 }
