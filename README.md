b44590d72fb4ee9ab571a2e8716594e540917ecd
Anna Lawley and Ramon de la Cruz <ALawley1@gmail.com>
2016-02-17 16:08:08 -0800

    add readme and integration tests

---
+++ README.md
@@ -0,0 +1,39 @@
+# _CD Organizer_
+
+#### _Epicodus Java Course OOP Exercise, 02.17.16_
+
+#### By _**Anna Lawley and Ramon de la Cruz**_
+
+## Description
+
+_This page will let a user create a list of CD titles as well as a list of artists. They can view both lists, or assign albums to individual artists, and then view all albums assigned to a given artist._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Install the [Java SDK](http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html) and [Java SRE](http://www.java.com/en/)._
+* _[Install gradle](http://codetutr.com/2013/03/23/how-to-install-gradle/)_
+* _Open a terminal, navigate to the root of the cloned directory, and enter "gradle run"._
+* _Open localhost:4567 in a browser._
+
+## Known Bugs
+
+_albumShowPageDisplaysTitle() test in AppTest.java fails, but page is fully functional and the behavior it aims to test works properly._
+
+## Support and contact details
+
+_To contact, leave a comment on Github._
+
+## Technologies Used
+
+* _Java_
+* _JUnit_
+* _FluentLenium_
+* _Spark_
+* _Gradle_
+
+### License
+
+*MIT License*
+
+Copyright (c) 2016 **_Anna Lawley and Ramon de la Cruz_**
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,15 +3,118 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
   public WebDriver getDefaultDriver() {
       return webDriver;
   }
 
+
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("CD Organizer!");
+  }
+
+  @Test
+  public void albumIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a new album"));
+    fill("#title").with("The Mantle");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your entry has been saved.");
+  }
+
+  @Test
+  public void albumIsDisplayedTest() {
+    goTo("http://localhost:4567/albums/new");
+    fill("#title").with("The Mantle");
+    submit(".btn");
+    click("a", withText("View all artists and albums"));
+    assertThat(pageSource()).contains("The Mantle");
+  }
+
+  @Test
+  public void artistIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a new artist"));
+    fill("#title").with("The Mantle");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your entry has been saved.");
+  }
+
+  @Test
+  public void artistIsDisplayedTest() {
+    goTo("http://localhost:4567/albums/new");
+    fill("#title").with("Agalloch");
+    submit(".btn");
+    click("a", withText("View all artists and albums"));
+    assertThat(pageSource()).contains("Agalloch");
+  }
+
+  @Test
+  public void multipleAlbumsAreDisplayedTest() {
+    goTo("http://localhost:4567/albums/new");
+    fill("#title").with("The Mantle");
+    submit(".btn");
+    goTo("http://localhost:4567/albums/new");
+    fill("#title").with("The Grey");
+    submit(".btn");
+    click("a", withText("View all artists and albums"));
+    assertThat(pageSource()).contains("The Mantle");
+    assertThat(pageSource()).contains("The Grey");
+  }
+
+  @Test
+  public void multipleArtistsAreDisplayedTest() {
+    goTo("http://localhost:4567/artists/new");
+    fill("#title").with("Agalloch");
+    submit(".btn");
+    goTo("http://localhost:4567/artists/new");
+    fill("#title").with("Stepdad");
+    submit(".btn");
+    click("a", withText("View all artists and albums"));
+    assertThat(pageSource()).contains("Agalloch");
+    assertThat(pageSource()).contains("Stepdad");
+  }
+
+  @Test
+  public void artistShowPageDisplaysName() {
+    goTo("http://localhost:4567/artists/new");
+    fill("#title").with("Agalloch");
+    submit(".btn");
+    click("a", withText("View all artists and albums"));
+    click("a", withText("Agalloch"));
+    assertThat(pageSource()).contains("Agalloch");
+  }
+
+  @Test
+  public void albumShowPageDisplaysTitle() {
+    goTo("http://localhost:4567/albums/new");
+    fill("#title").with("The Mantle");
+    submit(".btn");
+    click("a", withText("View all artists and albums"));
+    click("a", withText("The Mantle"));
+    assertThat(pageSource()).contains("The Mantle");
+  }
+
+  @Test
+  public void albumNotFoundMessageShown() {
+    goTo("http://localhost:4567/albums/999");
+    assertThat(pageSource()).contains("Album not found");
+  }
+
+  @Test
+  public void artistNotFoundMessageShown() {
+    goTo("http://localhost:4567/artists/999");
+    assertThat(pageSource()).contains("Artist not found");
+  }
 }
